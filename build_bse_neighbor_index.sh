ab941c7fd3d760a53a34979d5a72ee80f6526cc8
amanrai <aman.rai25@gmail.com>
2026-07-29 16:01:01 +0530

    Cap BSE neighbor build game scan

--- build_bse_neighbor_index.sh
+++ build_bse_neighbor_index.sh
@@ -52,4 +52,5 @@ uv run python scripts/build_bse_neighbor_index.py \
   --min-elo 1800 \
   --max-elo 2200 \
   --bucket-plies 5 \
-  --device cuda
+  --max-games "${BSE_NEIGHBOR_MAX_GAMES:-250000}" \
+  --device "${BSE_NEIGHBOR_DEVICE:-cuda}"
